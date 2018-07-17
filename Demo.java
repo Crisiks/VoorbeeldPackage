25a41da444d9c8a699b3700fc687c5c710785d77
Cris <crispijn.sleeboom@gmail.com>
2018-07-17 11:32:26 +0200

    commit 1

---
+++ Demo.java
@@ -0,0 +1,16 @@
+package dinsdag;
+
+import dinsdag.Bal;
+import dinsdag.scheidsrechter.Scheidsrechter;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Bal deBal = new Bal();
+	//	VoetbalSpeler vb = new VoetbalSpeler();
+		Scbeidsrechter sr = new Scheidsrechter();
+		deBal.schieten();
+		
+	}
+
+}
---
+++ scheidsrechter/Scheidsrechter.java
@@ -0,0 +1,10 @@
+package dinsdag.scheidsrechter;
+
+public class Scheidsrechter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
